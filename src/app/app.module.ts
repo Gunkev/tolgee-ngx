49932cb281887bf1d90d5125bb7eab5ce495edfb
gunkev <kevinenzapdi@gmail.com>
2023-07-04 03:46:25 +0100

    added tolgee config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  DevTools,
+  NgxTolgeeModule,
+  Tolgee,
+  TOLGEE_INSTANCE,
+  FormatSimple,
+} from '@tolgee/ngx';
+import { environment } from 'src/environments/environment.local';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, LanguageSelectorComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxTolgeeModule],
+  providers: [
+    {
+      provide: TOLGEE_INSTANCE,
+      useFactory: () => {
+        return Tolgee().use(DevTools()).use(FormatSimple()).init({
+          language: 'en',
+          apiUrl: environment.tolgeeApiUrl,
+          apiKey: environment.tolgeeApiKey,
+        });
+      },
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
